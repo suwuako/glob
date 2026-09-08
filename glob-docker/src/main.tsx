60c5bf42f0298978957533247c4506360039eb11
suwa <ronnielow@protonmail.com>
2026-09-08 16:35:21 +1000

    blog update

--- glob-docker/src/main.tsx
+++ glob-docker/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Tros from './tros.tsx'
+import Getting_code_from_an_npu from './getting-code-on-an-npu.tsx'
 import Rustracer from './rustracer.tsx'
 
 createRoot(document.getElementById('root')!).render(
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
         <Route index element={<App />} />
         <Route path="rustracer" element={<Rustracer />} />
         <Route path="tros" element={<Tros />} />
+        <Route path="getting-code-on-an-npu" element={<Getting_code_from_an_npu />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
